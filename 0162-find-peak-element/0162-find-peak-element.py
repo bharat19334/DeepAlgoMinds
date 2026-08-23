48b9ce962dabff951f4824829c3ff0b5def6eeb3
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-08-23 23:01:36 +0530

    Time: 0 ms (100%), Space: 19.3 MB (79.82%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,22 +3,21 @@ class Solution:
 
         # brute force
         n = len(nums)
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1]:
-                return i
-        return n - 1
+        # for i in range(len(nums) - 1):
+        #     if nums[i] > nums[i + 1]:
+        #         return i
+        # return n - 1
         
 
-        # left = 0
-        # right = n-1
+        left = 0
+        right = n-1
 
-        # while left<right:
-        #     mid = left + (right-left)//2
-        #     if nums[mid] < nums[mid+1]:
-        #         left = mid +1
-        #     else:
-        #         right = mid
-            
-        # return left
+        while left<right:
+            mid = left + (right-left)//2
+            if nums[mid] < nums[mid+1]:
+                left = mid +1
+            else:
+                right = mid
+        return left
 
 
