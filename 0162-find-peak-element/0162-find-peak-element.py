14028967e6df935174a1b3079323a583d53e4ca1
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-08-23 23:10:22 +0530

    Time: 0 ms (100%), Space: 19.3 MB (39.69%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,6 +3,11 @@ class Solution:
 
         
         n = len(nums)
+        # for i in range(0,n-1):
+        #     if nums[i] > nums[i+1]:
+        #         return i
+        # return n-1
+
         left = 0
         right = n-1
 
