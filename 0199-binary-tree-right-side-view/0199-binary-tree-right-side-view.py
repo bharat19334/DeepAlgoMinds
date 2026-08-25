5cd33428b37f38ef74db6142c8d5ad04d275bcb7
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-08-25 23:33:53 +0530

    Update 0199-binary-tree-right-side-view.py

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class Solution: 
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         
         
