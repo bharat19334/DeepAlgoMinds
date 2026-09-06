c3e1b7f13f188072b051c3ecd89bbfe210706ba7
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-09-06 13:18:05 +0530

    Time: 710 ms (5.04%), Space: 12.2 MB (89.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,6 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        if n <= 1:
+class Solution(object):
+    def fib(self, n):
+        
+        if n==0 or n==1:
             return n
         return self.fib(n-1) + self.fib(n-2)
