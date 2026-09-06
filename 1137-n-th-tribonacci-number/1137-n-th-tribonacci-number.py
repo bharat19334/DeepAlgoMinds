b5c725468344278ba950edcd987cbeacca3a57a4
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-09-07 00:28:14 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.21%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,18 +1,16 @@
 class Solution(object):
     def tribonacci(self, n):
         
-        if n == 0:
+        if n==0:
             return 0
         if n==1 or n==2:
             return 1
-
-        # return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
-
-        dp = [0]*(n+1)
-        dp[1] = 1
-        dp[2] = 1
-
-        for i in range(3,n+1):
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-            
-        return dp[n] 
+        t0 = 0
+        t1 = 1
+        t2 = 1
+        i=3
+        while i <= n:
+            t3 = t0+t1+t2
+            t0,t1,t2 = t1,t2,t3
+            i+=1
+        return t3
