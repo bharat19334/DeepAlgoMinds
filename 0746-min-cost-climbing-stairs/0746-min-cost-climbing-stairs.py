1e63dbf84af2afb598e3fc6c862439588c6ceefc
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-09-07 00:36:22 +0530

    Time: 0 ms (100%), Space: 12.4 MB (91.62%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,16 +1,13 @@
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
         
-        # Create a dp array of length n initialized with 0
-        dp = [0]*len(cost)
-        # we have minimum cost to reach the first two stairs
-        dp[0] = cost[0]
-        dp[1] = cost[1]
-        n = len(cost)
-
-        for i in range(2,n):
-            # current cost + from previous two stairs minimum cost
-            dp[i] = cost[i] + min(dp[i-2],dp[i-1])
-
-        return min(dp[n-1],dp[n-2])
-
+        first = 0
+        second = 0
+        
+        for i in range(2, len(cost) + 1):
+            
+            current = min(second + cost[i-1], first + cost[i-2] )
+            first = second
+            second = current
+            
+        return second
