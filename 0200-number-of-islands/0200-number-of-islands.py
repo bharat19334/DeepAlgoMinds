f2b63b58eafbe816886c4ba1e50f4bc173ebde01
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-09-11 00:54:40 +0530

    Time: 196 ms (97.68%), Space: 27.9 MB (48.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def numIslands(self, grid):
         
-        m = len(grid)
-        n = len(grid[0])
+        r_len = len(grid)
+        c_len = len(grid[0])
         count = 0
 
-        for i in range(m):
-            for j in range(n):
+        for i in range(r_len):
+            for j in range(c_len):
 
                 if grid[i][j] == "1":
                     count += 1
-
                     stack = [(i, j)]
                     grid[i][j] = "0"
 
@@ -21,7 +20,7 @@ class Solution(object):
                             grid[row-1][col] = "0"
                             stack.append((row-1, col))
 
-                        if row < m-1 and grid[row+1][col] == "1":
+                        if row < r_len-1 and grid[row+1][col] == "1":
                             grid[row+1][col] = "0"
                             stack.append((row+1, col))
 
@@ -29,7 +28,7 @@ class Solution(object):
                             grid[row][col-1] = "0"
                             stack.append((row, col-1))
 
-                        if col < n-1 and grid[row][col+1] == "1":
+                        if col < c_len-1 and grid[row][col+1] == "1":
                             grid[row][col+1] = "0"
                             stack.append((row, col+1))
 
